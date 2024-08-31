fc59eff25421a19f3b709563e45e8f48b00dd128
asamanipeterson <161303282+asamanipeterson@users.noreply.github.com>
2024-08-31 11:08:31 +0000

    Add files via upload

---
+++ Sign-Up.js
@@ -0,0 +1,16 @@
+function myFunction(){
+    var x=document.getElementById('myInput');
+    var y=document.getElementById('hide');
+    var z=document.getElementById('show');
+
+    if(x.type==='password'){
+        x.type='text';
+        y.style.display='none';
+        z.style.display='block';
+    }
+    else{
+        x.type='password';
+        y.style.display='block';
+        z.style.display='none';
+    }
+}
